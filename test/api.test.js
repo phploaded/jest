c8a147959a0be14a31d474126fad5c2e63f3a046
phploaded <satish@phploaded.com>
2024-07-23 21:55:16 +0530

    upd

--- test/api.test.js
+++ test/api.test.js
@@ -1,56 +1,66 @@
-// //const Ajv = require("ajv");
-// const request = require('supertest');
+require("dotenv").config();
+//console.log(process.env.API_BASE_URL);
 
-//  test('API GET Request Test', async () => {
-//   const response = await request('https://jsonplaceholder.typicode.com/posts/1')
-//   .then((res) => res.json());
-
-//   console.log("**********Hellow World****");
-//   console.log(response);
-  
-//   //expect(response.status).toBe(200);
-//   //expect(response.body.userId).toBe(1);
-// });
-
-
-// describe(`GET https://jsonplaceholder.typicode.com/posts/1`, () => {
-//   let response;
-//   let body;
-// console.log();
-// });
-
-// test({
-//   response = fetch('https://jsonplaceholder.typicode.com/posts/1');
-// console.log(response);
-// });
-
-// it('descr', () => {
-//  });
-/*
-(xcode) => x*y
-(xcode) => {code}
-*/
-// it('descr', async mytest(code));
-// describe('descr', mytest(code));
-describe('Test case 1', () => {
-  let jsondata;
-  // it('Checking if THETIMES exists in publishers', async () => {
-  //   const response = await fetch('http://localhost/narendra/publishers.json');
-  //   const data = await response.json();
-  //   //console.log(data.publishers[0]);
-  //   expect(data.publishers[0]).toBe('THETIMES'); 
+let jsondata;
 beforeAll(async() =>{
-    const response = await fetch('http://localhost/narendra/publishers.json');
+  const url = `${process.env.API_BASE_URL}?api_key=${process.env.API_KEY}`;
+  console.log(url);
+    const response = await fetch(url); // full url should be https://api.themoviedb.org/3/movie/550?api_key=3bf33331bf07dfd455b91852342c5b1f
     const data = await response.json();
-    //console.log(data.publishers[0]);
-    //expect(data.publishers[0]).toBe('THETIMES'); 
-    // console.log(data);
     jsondata = data;
   });
+describe('Account Module', () => {
 
-  console.log(jsondata);
+
+
 
   it('Verify ', ()=>{
+    console.log(jsondata);
+    expect(jsondata.budget).toBe(63000000);
+
+  });
+
+  it('Verify ', ()=>{
+    console.log(jsondata);
+    expect(jsondata.publishers[0]).toBe('THETIMES');
+
+  });
+
+});
+
+describe('Payment Module', () => {
+
+
+
+
+  it('Verify ', ()=>{
+    console.log(jsondata);
+    expect(jsondata.budget).toBe(63000000);
+
+  });
+
+  it('Verify ', ()=>{
+    console.log(jsondata);
+    console.log(jsondata);// //
+    expect(jsondata.publishers[0]).toBe('THETIMES');
+
+  });
+
+});
+
+describe('Product Module', () => {
+
+
+
+
+  it('Verify ', ()=>{
+    console.log(jsondata);
+    expect(jsondata.budget).toBe(63000000);
+
+  });
+
+  it('Verify ', ()=>{
+    console.log(jsondata);
     expect(jsondata.publishers[0]).toBe('THETIMES');
 
   });
